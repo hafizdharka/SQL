130bbe29b48c80a94eae568018146f13b8a91284
Hafizd Harka <143497383+hafizdharka@users.noreply.github.com>
2024-04-17 02:46:58 +0700

    Update 197.sql
    cross product

--- LeetCode/197.sql
+++ LeetCode/197.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
-SELECT w1.id As ID
-FROM Weather w1
-LEFT JOIN Weather w2
-ON w1.id=w2.id+1
-WHERE w1.temperature>w2.temperature
+SELECT w1.id as Id
+FROM Weather w1, Weather w2
+WHERE dateDiff(w1.recordDate,w2.recordDate) = 1 AND w1.Temperature > w2.Temperature
+
