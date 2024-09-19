b9c2b2f6b2eb2902875280aa244fb780bcf3017e
Harkaputra <143497383+hafizdharka@users.noreply.github.com>
2024-09-19 10:21:40 +0700

    Create 180 - Consecutive Numbers.sql

---
+++ LeetCode/easy/advanced select and joins/180 - Consecutive Numbers.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select distinct num as ConsecutiveNums
+from (
+    select num,
+        lag(num,1) over (order by id) as one,
+        lag(num,2) over (order by id) as two
+    from logs
+) cons
+where num=one and num=two
