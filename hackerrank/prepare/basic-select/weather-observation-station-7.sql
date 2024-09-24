790e0153ea00b53d1c0e9f03fb30ae205005efc7
Harkaputra <143497383+hafizdharka@users.noreply.github.com>
2024-09-24 16:24:27 +0700

    Create weather-observation-station-7.sql

---
+++ hackerrank/prepare/basic-select/weather-observation-station-7.sql
@@ -0,0 +1,12 @@
+select distinct(city)
+from station
+where city like '%a'
+    or city like '%i'
+    or city like '%u'
+    or city like '%e'
+    or city like '%o'
+    or city like '%A'
+    or city like '%I'
+    or city like '%U'
+    or city like '%E'
+    or city like '%O'
